a9c21d4b12c5468afbd7c15c2b65aaac4fed16c0
qazybi <k.askarbek@innopolis.university>
2020-09-22 22:36:30 +0300

    json parsing added

--- WordEnum.java
+++ WordEnum.java
@@ -11,6 +11,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+
 public class WordEnum {
 
   public static class TokenizerMapper
@@ -21,14 +26,18 @@ public class WordEnum {
 
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
+      try{
+	  JSONObject article_json = new JSONObject(value.toString());
+	  StringTokenizer itr = new StringTokenizer(article_json.get("title").toString());
+	  while (itr.hasMoreTokens()) {
+          	word.set(itr.nextToken());
+          	context.write(word, one);
+     	  }
+      }catch(JSONException e){
+	     e.printStackTrace();
       }
-    }
-  }
-
+   }
+}
   public static class IntSumReducer
        extends Reducer<Text,IntWritable,Text,IntWritable> {
     private IntWritable result = new IntWritable();
@@ -54,8 +63,10 @@ public class WordEnum {
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
+
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
+
