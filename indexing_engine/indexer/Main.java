66fe2661764b539aa0cbceb8eab3b6e3045ff6fd
DinarZayahov <dinar.zayaxov@mail.ru>
2020-09-24 23:37:33 +0300

    Added methods for Relevance Analizator

--- indexing_engine/indexer/Main.java
+++ indexing_engine/indexer/Main.java
@@ -86,9 +86,30 @@ public class Main
 	}
     }
 
-//	public static Map<IntWritable, FloatWritable> inner_product(String[] IndTextLines, FloatWritable query_vector){
-//		return 
-//	}
+	public static Map<IntWritable, FloatWritable> inner_product(String[] IndTextLines, ArrayList<FloatWritable> query_vector){
+        Map<IntWritable, FloatWritable> relevance_map = new HashMap<IntWritable, FloatWritable>();
+        Integer len = IndTextLines.length;
+        for (int i=0; i<len; i++){
+            String[] dcElements = dcTextLines[i].split("\t");
+            ArrayList<FloatWritable> vector = map.get(dcElements[1]);
+            FloatWritable relevance = relevance(vector, query_vector);
+            relevance_map.put(dcElements[0], relevance);
+        }
+        return relevance_map;
+    }
+
+    public static FloatWritable relevance(ArrayList<FloatWritable> arr1, ArrayList<FloatWritable> arr2){
+        Integer len = arr1.size();
+        for (int i=0;i<len;i++){
+            FloatWritable p = arr1.get(i)*arr2.get(i);
+            arr1.set(i, p);
+        }
+        Integer sum = 0;
+        for (int i=0;i<len;i++){
+            sum = sum + arr1.get(i);
+        }
+        return sum
+    }
 
     public static void main(String[] args) {}
 }
